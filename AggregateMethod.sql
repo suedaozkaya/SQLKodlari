c6ceca4aea25d900ba54af7bca5395b50378839e
Süeda Özkaya <sozkaya2635@gmail.com>
2022-08-26 23:35:00 +0300

    26.08.2022

---
+++ AggregateMethod.sql
@@ -0,0 +1,54 @@
+-- AGGREGATE METHOD KUALLANIMI
+/*
+-Aggregate Methodları (SUM,COUNT,MIN,MAX,AVG)
+-Subquery içinde de kullanılır
+-Ancak sorgu tek bir değer döndürüyor olmalıdır
+SYNTAX: sum() şeklinde olmalı sum () arasında boşluk olmamalı
+*/
+
+SELECT * FROM calisanlar2;
+
+--Çalışanlar2 tablosundaki en yüksek maaş değerini listeleyiniz
+SELECT MAX(maas) FROM calisanlar2;
+
+--calisanlar2 tablosundaki maaslarin toplamını listeleyiniz
+SELECT SUM(maas) FROM calisanlar2;
+
+--calisanlar2 tablosundaki maas ortalamasını listeleyiniz
+SELECT AVG(maas) FROM calisanlar2;
+SELECT round(AVG(maas),2) FROM calisanlar2;
+
+--calisanlar2 tablosundaki en düşük maaşı listeleyiniz
+SELECT MIN(maas) FROM calisanlar2;
+
+--calisanlar2 tablosundaki kaç kişinin maaş aldığını listeleyiniz
+SELECT COUNT(maas) FROM calisanlar2;
+
+SELECT * FROM calisanlar2;
+SELECT * FROM markalar;
+-- Her markanin ismini, calisan sayisini ve o markaya ait calisanlarin toplam maaşini listeleyiniz
+SELECT marka_isim, calisan_sayisi, (SELECT SUM(maas) FROM calisanlar2 
+WHERE marka_isim = isyeri) AS toplam_maas FROM markalar;
+
+-- ALIAS (AS) --> Tabloda geçici isim vermek istersek koşuldan sonra AS sutun_isim olarak kullanılır.
+
+-- Her markanin ismini, calisan sayisini ve o markaya ait calisanlarin maksimum ve minumum maaşini listeleyen bir Sorgu yaziniz.
+SELECT marka_isim, calisan_sayisi, (SELECT MAX(maas) FROM calisanlar2 WHERE marka_isim = isyeri) AS max_maas,
+                                   (SELECT MIN(maas) FROM calisanlar2 WHERE marka_isim = isyeri) AS min_maas FROM markalar;
+								   
+-- Her markanin id’sini, ismini ve toplam kaç şehirde bulunduğunu listeleyen bir SORGU yaziniz.
+SELECT marka_id, marka_isim, (SELECT COUNT(sehir) FROM calisanlar2 WHERE marka_isim = isyeri) AS sehir_sayisi FROM markalar;
+
+--Interview Question: En yüksek ikinci maas değerini çağırın.
+SELECT MAX(maas) AS enyuksek_ikinci_maas FROM calisanlar2 
+WHERE maas<(SELECT MAX(maas) FROM calisanlar2);
+
+----Interview Question: En düşük ikinci maas değerini çağırın.
+SELECT MIN(maas) AS endusuk_ikinci_maas FROM calisanlar2 
+WHERE maas>(SELECT MIN(maas) FROM calisanlar2);
+
+--En yüksek üçüncü maas değerini bulun
+SELECT MAX(maas) AS enyuksek_ucuncu_maas FROM calisanlar2 
+WHERE maas<(SELECT MAX(maas) FROM calisanlar2 WHERE maas<(SELECT MAX(maas) FROM calisanlar2));
+
+--En düşük üçüncü maaş değerini bulunuz (ÖDEV)
