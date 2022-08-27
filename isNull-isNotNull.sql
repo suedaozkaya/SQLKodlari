294264257c82c5d25362612a25175aa09a48c19c
Süeda Özkaya <sozkaya2635@gmail.com>
2022-08-27 22:55:56 +0300

    27.08.2022

--- isNull-isNotNull.sql
+++ isNull-isNotNull.sql
@@ -25,3 +25,32 @@ SELECT * FROM insanlar;
 
 -- ismi null olanları sorgula
 SELECT * FROM insanlar WHERE isim IS NULL;
+
+-- null olmayanları listeleyelim
+SELECT * FROM insanlar WHERE isim IS NOT NULL;
+
+-- isim 'i NULL olan kişilerin isim'ine NO NAME atayınız.
+UPDATE insanlar
+SET isim = 'NO NAME'
+WHERE isim IS null;
+
+-- Tabloyu bir önceki soruyu çözmeden önceki haline geri getirin
+UPDATE insanlar
+SET isim = null
+WHERE isim IS 'NO NAME';
+
+-- NOT: Çoklu değişimde her değişim için SET ... WHERE isim IS NULL gibi
+-- ifade yazmamak için COALESCE(birleştirme) kullanılır
+
+/* 
+isim 'i NULL olanlara 'Henuz isim girilmedi'
+adres 'i NULL olanlara 'Henuz adres girilmedi'
+ssn 'i NULL olanlara 'no ssn' atayalım.
+*/
+
+UPDATE insanlar
+SET isim = COALESCE (isim,'Henuz isim girilmedi'),
+    adres = COALESCE (adres,'Henuz adres girilmedi'),
+	ssn = COALESCE (ssn,'no ssn');
+	
+SELECT * FROM insanlar;
